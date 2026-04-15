3a0ee3884ce6638b108c47e01d349f809b089eec
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-04-15 18:48:12 +0300

    aaa

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -4,6 +4,12 @@ const LEVEL_WIDTH := 60
 const LEVEL_HEIGHT := 30
 const CAMERA_SPEED := 400.0
 
+const TREE_COUNT := 30
+const TREE_AREA_WIDTH := 20
+const TREE_AREA_HEIGHT := 15
+
+const TreeScene = preload("res://Scenes/Tree.tscn")
+
 @onready var grass_layer: TileMapLayer = $Grass
 @onready var camera: Camera2D = $Camera2D
 
@@ -18,6 +24,25 @@ func _ready() -> void:
 	camera.limit_right = LEVEL_WIDTH * tile_size.x
 	camera.limit_bottom = LEVEL_HEIGHT * tile_size.y
 	camera.position = get_viewport_rect().size / 2
+
+	_spawn_trees()
+
+func _spawn_trees() -> void:
+	var tile_size := grass_layer.tile_set.tile_size
+	var start_x := randi_range(0, LEVEL_WIDTH - TREE_AREA_WIDTH)
+	var start_y := randi_range(0, LEVEL_HEIGHT - TREE_AREA_HEIGHT)
+
+	var positions: Array[Vector2i] = []
+	for x in TREE_AREA_WIDTH:
+		for y in TREE_AREA_HEIGHT:
+			positions.append(Vector2i(start_x + x, start_y + y))
+	positions.shuffle()
+
+	for i in min(TREE_COUNT, positions.size()):
+		var tile_pos := positions[i]
+		var tree := TreeScene.instantiate()
+		tree.position = grass_layer.map_to_local(tile_pos) + Vector2(0, tile_size.y / 2.0)
+		add_child(tree)
 
 func _process(delta: float) -> void:
 	var direction := Vector2.ZERO
