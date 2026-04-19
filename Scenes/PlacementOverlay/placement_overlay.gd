e04a0a7671fa792a7b04b2bc6db4c00faefc504f
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2026-04-19 16:14:31 +0300

    aa

--- Scenes/PlacementOverlay/placement_overlay.gd
+++ Scenes/PlacementOverlay/placement_overlay.gd
@@ -2,15 +2,15 @@ class_name PlacementOverlay
 extends Node2D
 
 var _invalid_rects: Array[Rect2] = []
-var _arrow_world_pos: Vector2 = Vector2.ZERO
+var _arrow_dir: Vector2 = Vector2.ZERO
 var _show_arrow: bool = false
 
 func show_rects(rects: Array[Rect2]) -> void:
 	_invalid_rects = rects
 	queue_redraw()
 
-func set_arrow(world_pos: Vector2, enabled: bool) -> void:
-	_arrow_world_pos = world_pos
+func set_arrow(direction: Vector2, enabled: bool) -> void:
+	_arrow_dir = direction
 	_show_arrow = enabled
 	queue_redraw()
 
@@ -29,11 +29,10 @@ func _draw() -> void:
 		var sp := vt * rect.position
 		draw_rect(Rect2(sp, rect.size * sc), color)
 
-	if _show_arrow:
+	if _show_arrow and _arrow_dir != Vector2.ZERO:
 		var vp_size := get_viewport_rect().size
 		var center := vp_size * 0.5
-		var target_screen := vt * _arrow_world_pos
-		var dir := (target_screen - center).normalized()
+		var dir := _arrow_dir.normalized()
 		var tip := center + dir * 100.0
 		draw_line(center, tip, Color.YELLOW, 4.0)
 		var ang := dir.angle()
